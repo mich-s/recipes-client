f80a20726cf6dcad01d8964e9243b7041ff1bf39
michal <michals22@gazeta.pl>
2018-09-25 13:56:06 +0200

    tweak dto

--- src/main/java/com/michal/recipesclient/dto/RecipeDTO.java
+++ src/main/java/com/michal/recipesclient/dto/RecipeDTO.java
@@ -48,6 +48,46 @@ public class RecipeDTO {
 	public String toString() {
 		return "Recipe [id=" + id + ", name=" + name + ", method=" + method + ", ingredients=" + ingredients + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((ingredients == null) ? 0 : ingredients.hashCode());
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecipeDTO other = (RecipeDTO) obj;
+		if (id != other.id)
+			return false;
+		if (ingredients == null) {
+			if (other.ingredients != null)
+				return false;
+		} else if (!ingredients.equals(other.ingredients))
+			return false;
+		if (method == null) {
+			if (other.method != null)
+				return false;
+		} else if (!method.equals(other.method))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 	
 	
